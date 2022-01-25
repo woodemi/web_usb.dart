ef2a9db29b9f3335c1f9073333b1eaaed9e90a8c
wangkun <wangkun@woodemi.com>
2022-01-25 08:33:22 +0800

    Initial commit
    Signed-off-by: wangkun <wangkun@woodemi.com>

---
+++ example/web_usb_example.dart
@@ -0,0 +1,6 @@
+import 'package:web_usb/web_usb.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ test/web_usb_test.dart
@@ -0,0 +1,16 @@
+import 'package:web_usb/web_usb.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('A group of tests', () {
+    final awesome = Awesome();
+
+    setUp(() {
+      // Additional setup goes here.
+    });
+
+    test('First Test', () {
+      expect(awesome.isAwesome, isTrue);
+    });
+  });
+}
